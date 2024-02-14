6716c945fcfdd272f87fac241c4f1f91172635d0
Clara Chen <jiaxc1@uci.edu>
2024-02-13 22:10:08 -0800

    Edited functions

--- scraper.py
+++ scraper.py
@@ -15,6 +15,7 @@ unique_urls = set()  # all the visited urls, without the fragment
 stop_words = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
 fingerprint_set = set() # set of all the fingerprints of visited sites
 checksum_set = set()
+robots_checked = set()
 max_redirects = 20
 
 
@@ -44,14 +45,18 @@ def write_result():
 
         file.write("3. 50 Most Common Words:\n")
         for word, frequency in top_50_words:
-            file.write(f"{word}:, Frequency: {frequency}")
+            file.write(f"{word}: {frequency}")
             file.write("\n")
         file.write("\n")
 
-        file.write("4. Subdomains Found:\n")
-        file.write(str(len(subdomain_dict.keys())))
-        file.write("\n")
 
+        file.write("4. Subdomains Found:\n")
+        file.write(str(len(subdomain_dict)) + "\n")
+
+        # Iterate over each subdomain and its corresponding value, sorted alphabetically
+        for subdomain, value in sorted(subdomain_dict.items()):
+            file.write(f"{subdomain}: {value}\n")
+        file.write("\n")
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -77,11 +82,37 @@ def extract_next_links(url, resp, redirects_followed=0):
     #         redirects_followed += 1
     #         return extract_next_links(url, resp, redirects_followed)
 
-    
     # if redirects_followed == max_redirects:
     #     return []
 
     if is_valid(url) and resp.status == 200:
+        # # check if this url is a subdomain
+        # if url not in robots_checked and (url.endswith("ics.uci.edu") or url.endswith("cs.uci.edu") or url.endswith("informatics.uci.edu") or url.endswith("stat.uci.edu")):
+        #     print("added robot link")
+        #     robots_checked.add(url)
+        #     url += "/robots.txt"
+        #     return [url]
+
+        # if "robots.txt" in url:
+        #     soup = BeautifulSoup(resp.raw_response.content, "html.parser")
+    
+        #     # Find all lines containing the sitemap link
+        #     sitemap_lines = [line for line in soup.get_text().split("\n") if "Sitemap:" in line]
+            
+        #     # Extract the sitemap URL from the first line
+        #     if sitemap_lines:
+        #         sitemap_url = sitemap_lines[0].split("Sitemap:")[1].strip()
+        #         print(sitemap_url)
+        #         return [sitemap_url]
+        #     else:
+        #         print("none")
+        #         url = url.replace("/robots.txt", "")
+        #         print("replaced url", url)
+        #         return [url]
+
+        # if url.endswith(".xml"):
+        #     soup = BeautifulSoup(resp.raw_response.content, "html.parser")
+
         # finding the largest document
         soup = BeautifulSoup(resp.raw_response.content, "html.parser")
         visited_urls.add(url)
@@ -90,7 +121,7 @@ def extract_next_links(url, resp, redirects_followed=0):
         page_dict, token_list = tokenize_text(soup.get_text())
 
         # dont crawl if page has too little or too much information
-        if len(token_list) < 50 or len(token_list) > 5000:
+        if len(token_list) < 50 :
             return []
 
         # updates the largest file
@@ -126,7 +157,12 @@ def extract_next_links(url, resp, redirects_followed=0):
                     fingerprint_set.add(fingerprint)
                     near_duplicate = True
                     break
-                    
+
+        if fingerprint in fingerprint_set:
+            print("fingerprint exists: ", fingerprint)     
+        if near_duplicate:
+            print("near_duplicate true")     
+
         # check if fingerprint already exists to detect exact similarity or if there is near similarity
         if (fingerprint not in fingerprint_set) and not near_duplicate:        
             a_tags = soup.find_all("a", href=True)
@@ -149,7 +185,6 @@ def extract_next_links(url, resp, redirects_followed=0):
             fingerprint_set.add(fingerprint)      
 
     write_result()
-
     return links
 
 
@@ -161,10 +196,10 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        if ((".ics.uci.edu" not in url) and (".cs.uci.edu" not in url) and (".informatics.uci.edu" not in url) and (".stat.uci.edu" not in url)):
+        if (("ics.uci.edu" not in url) and ("cs.uci.edu" not in url) and ("informatics.uci.edu" not in url) and ("stat.uci.edu" not in url)):
             return False
         return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            r".*\.(ppsx|css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
@@ -207,13 +242,17 @@ def tokenize_text(text: str) -> None:
     Tokenize the text from the document. Find the top 50 words.
     """
     lowercase_content = text.lower()  # convert all words to lowercase
-    tokens = re.split(r"[^\w'-]", lowercase_content.strip())  # tokens can only be alphanumeric
+    tokens = re.split(r"[^\w\-'\.]", lowercase_content.strip()) # split by alphabets, nums, hyphens, apostrophes, and periods
+
     token_list = list(filter(None, tokens))
     page_dict = {}
 
     try:
         # iterate through all tokens to find frequencies; find top 50 words
         for token in token_list:
+            # remove the last period if the token ends with one
+            if token.endswith('.') and len(token) > 1:
+                token = token[:-1]
             if token not in stop_words and len(token) > 1:
                 if token in token_frequency:  # if the key exists, increment its frequency
                     token_frequency[token] += 1
@@ -247,7 +286,6 @@ def checksum(tokens):
     for token in tokens:
         for char in token:
             sum += ord(char)
-        sum &= 0xFF
     return sum
 
 
@@ -255,36 +293,23 @@ def simhash(page_dict: dict):
     """
     Detect similar documents. page_dict is every single word on a page with its frequency.
     """
-    # tokenizing: steps a + b
-    word_hashes = {word: hash_word(word) for word in page_dict.keys()} # step c
-    summing_weights = []  # step d
+    # Hash each word in the page_dict
+    word_hashes = {word: hash_word(word) for word in page_dict.keys()}
+    # Initialize fingerprint with 16 bits set to 0
+    fingerprint = [0] * 16   
 
-    # iterate through each words in page_dict
-    # access each index of each word to generate a vector of summing_weights
-    # general idea: word[index] + or - weight
-        # + if index is 1
-        # - if index is 0
-    # add all of the indexes of each word to summing_weights
-    for index in range(16):
-        sum = 0
-        for word in page_dict.keys():
-            # if the index is 0, subtract the word's weight to sum
-            if str(word_hashes[word])[index] == "0":
-                sum -= page_dict[word] 
-            # if the index is 1, add the word's weight to sum
-            elif str(word_hashes[word])[index] == "1":
-                sum += page_dict[word] 
-        summing_weights.append(sum)
+    # Combine hashes using XOR
+    for word, hash_value in word_hashes.items():
+        for i in range(16):  # Iterate over each bit position
+            # Extract i-th bit from the hash value
+            bit = (int(hash_value) // (2 ** i)) % 2
+            # Update fingerprint using XOR
+            fingerprint[i] ^= bit * page_dict[word]
 
-    fingerprint = []
+    # Convert fingerprint to binary string
+    fingerprint_str = ''.join(map(str, fingerprint))
 
-    # convert the summing weights to a binary fingerprint (step e)
-    for i in range(16):
-        if summing_weights[i] > 0:
-            fingerprint.append("1")
-        else:
-            fingerprint.append("0")
-    
-    fingerprint = "".join(fingerprint)
-    print(fingerprint)
-    return fingerprint
+    return fingerprint_str
+
+
+ 
